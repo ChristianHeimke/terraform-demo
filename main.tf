ef73b8a28cbd8bcb9f79ccda5b2ea75d98d9d7ea
Christian Heimke <christian.heimke@loumaris.com>
2022-10-25 11:07:11 +0200

    added stroage account

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
 # deklaration der ressourcen gruppe und location
 locals {
-  Ressouce_Groupe_Name = "christian_heimke_rg_${formatdate("YYYY-MM-DD",timestamp())}"
-  Ressouce_Groupe_Location = "West Europe"
+  Ressource_Group_Name = "christian_heimke_rg_${formatdate("YYYY-MM-DD",timestamp())}"
+  Ressource_Group_Location = "West Europe"
 }
 
 
 ## resourcen gruppe erstellen
 resource "azurerm_resource_group" "christian_heimke_rg" {
-  name     = local.Ressouce_Groupe_Name
-  location = local.Ressouce_Groupe_Location
+  name     = local.Ressource_Group_Name
+  location = local.Ressource_Group_Location
 }
+
+
+## erstellen des storage accounts
+resource "azurerm_storage_account" "demo_account" {
+  name                     = "demostorageaccount"
+  resource_group_name      = local.Ressource_Group_Name
+  location                 = local.Ressource_Group_Location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  depends_on = [
+    azurerm_resource_group.christian_heimke_rg
+  ]
+}
+
