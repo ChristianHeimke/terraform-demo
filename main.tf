18b983f5ac8e546c34f0dc010c152f004ad69754
Christian Heimke <christian.heimke@loumaris.com>
2022-10-25 11:04:33 +0200

    create ressource group

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+# deklaration der ressourcen gruppe und location
 locals {
   Ressouce_Groupe_Name = "christian_heimke_rg_${formatdate("YYYY-MM-DD",timestamp())}"
   Ressouce_Groupe_Location = "West Europe"
+}
+
+
+## resourcen gruppe erstellen
+resource "azurerm_resource_group" "christian_heimke_rg" {
+  name     = local.Ressouce_Groupe_Name
+  location = local.Ressouce_Groupe_Location
 }
